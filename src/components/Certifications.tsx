12f9fb58c16af67ffae98dd0944fc7a98ad0e24a
Rashaad Jones <92348662+rashaad367@users.noreply.github.com>
2024-09-29 19:20:31 -0400

    Update Certifications.tsx

--- src/components/Certifications.tsx
+++ src/components/Certifications.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import testout from "../assets/testoutsecpro.png";
+import comptiaaplus from "../assets/comptiaaplus.png";
 
 const Certifications: React.FC = () => {
   return (
     <>
       <h1 className="sub-heading container-lg">Certifications</h1>
+      <div className="container-lg flex border">
+        <img src={comptiaaplus} alt="Testout cert from school" width="150" />
+      </div>
       <div className="container-lg flex border">
         <img src={testout} alt="Testout cert from school" width="150" />
       </div>
