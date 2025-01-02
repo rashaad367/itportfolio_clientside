ec83ea88b50f3567a81decfcd042d8ffd9927236
Rashaad Jones <92348662+rashaad367@users.noreply.github.com>
2025-01-01 22:12:23 -0500

    Update ProjectCards.tsx

--- src/components/ProjectCards.tsx
+++ src/components/ProjectCards.tsx
@@ -11,7 +11,6 @@ type CardProps = {
 
 const Card: React.FC<CardProps> = ({ name, imageUrl, description, button1Text, link }) => {
   return (
-        <div className="container-lg border">
       <div className={style.cardBackground}>
         <div className={style.pcard}>
           <h1 className="font-mono center leading-loose text-white font-bold">{name}</h1>
@@ -23,7 +22,6 @@ const Card: React.FC<CardProps> = ({ name, imageUrl, description, button1Text, l
             </a>
           </div>
         </div>
-      </div>
     </div>
   );
 };
