827baf9c1e462838cfa0b3775deca5b904240e83
Rashaad Jones <92348662+rashaad367@users.noreply.github.com>
2025-01-01 22:21:30 -0500

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,16 +6,8 @@ const Projects: React.FC = () => {
   return (
     <>
       <h2 className="sub-heading container-lg" data-aos="fade-in" data-aos-duration="3000">
-        Projects
+        Blog
       </h2>
-      <div className="container-lg flex justify-evenly">
-          <ProjectCards
-            name="Helpdesk Project"
-            imageUrl={image1}
-            description="A lab dedicated to learning Helpdesk workflows."
-            button1Text="View"
-            link="/project1"
-          />
           <ProjectCards
             name="My Blog"
             imageUrl={image1}
