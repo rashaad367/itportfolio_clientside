7d31e8e54b40c3d441410e678715e9a6647892e1
Rashaad Jones <92348662+rashaad367@users.noreply.github.com>
2026-02-02 13:52:01 -0500

    Update Certifications.tsx

--- src/components/Certifications.tsx
+++ src/components/Certifications.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import testout from "../assets/testoutsecpro.png";
 import comptiaaplus from "../assets/comptiaaplus.png";
+import ccna from "../assets/ccna.png";
 
 const Certifications: React.FC = () => {
   return (
     <>
       <h1 className="sub-heading container-lg">Certifications</h1>
       <div className="container-lg flex border">
-        <img src={comptiaaplus} alt="Testout cert from school" width="100" height="100" className="w-1/9 mx-4 my-4"/>
-        <img src={testout} alt="Testout cert from school" width="100" height="100" className="w-1/9 mx-4 my-4"/>
+        <img src={comptiaaplus} alt="A+ Certification" width="100" height="100" className="w-1/9 mx-4 my-4"/>
+        <img src={testout} alt="Testout Certification" width="100" height="100" className="w-1/9 mx-4 my-4"/>
+        <img src={ccna} alt="CCNA Certification" width="100" height="100" className="w-1/9 mx-4 my-4"/>
       </div>
     </>
   );
